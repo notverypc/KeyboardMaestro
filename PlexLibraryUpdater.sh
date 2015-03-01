ef79f82d8b9f63884eacd1a94d1598817c77b070
notverypc <dad@TheIntersect.local>
2015-03-01 19:54:48 +0000

    Plex Updater

---
+++ PlexLibraryUpdater.sh
@@ -0,0 +1,5 @@
+# Plex Library Update
+# TV Shows
+do shell script "/Applications/Plex\\ Media\\ Server.app/Contents/MacOS/Plex\\ Media\\ Scanner --refresh --scan --section 2"
+# Movies
+do shell script "/Applications/Plex\\ Media\\ Server.app/Contents/MacOS/Plex\\ Media\\ Scanner --refresh --scan --section 1"
